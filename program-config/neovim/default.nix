2ff80c22b5f69ee489489b51a3f43791bbd511d9
Miauwkeru <Miauwkeru@users.noreply.github.com>
2023-07-21 20:08:36 +0200

    Add basic lazynvim config

---
+++ program-config/neovim/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      lazy-nvim
+    ];
+    extraLuaConfig = builtins.readFile ./lazy.lua;
+  };
+}
+

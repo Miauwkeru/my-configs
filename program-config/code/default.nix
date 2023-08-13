f5e660957d26726e7460f2cc4af30977484b2781
Miauwkeru <Miauwkeru@users.noreply.github.com>
2023-08-13 17:53:15 +0000

    Use podman in vscode instead of docker

--- program-config/code/default.nix
+++ program-config/code/default.nix
@@ -1,23 +1,36 @@
 { pkgs, config, ... }:
+let 
+  podman-wrapper = pkgs.writeScriptBin "podman-wrapper" ''
+  #!/bin/sh
+  BUILDAH_FORMAT=docker \
+  PODMAN_USERNS=keep-id \
+  podman "$@"
+  '';
+in 
 {
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
+      eamodio.gitlens
+      jnoortheen.nix-ide
       ms-azuretools.vscode-docker
       ms-python.python
       ms-python.vscode-pylance
-      eamodio.gitlens
+      vscodevim.vim
     ] ++ (
       if (config.gtk.theme.name == "Dracula" )
       then [
         dracula-theme.theme-dracula
-       ] else []
+      ] else []
     );
     userSettings = builtins.fromJSON (
       builtins.readFile ./settings.json
     ) // {
       "workbench.preferredDarkColorTheme" = "${config.gtk.theme.name}";
       "workbench.colorTheme" = "${config.gtk.theme.name}";
+    } // {
+      "dev.containers.dockerPath" = "${podman-wrapper}/bin/podman-wrapper";
+      "editor.rulers" = [80 120];
     };
   };
 }
